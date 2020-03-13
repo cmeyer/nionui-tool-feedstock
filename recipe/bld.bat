64a864fb16a383f9c58dac741b32f9d850a13053
Chris Meyer <cmeyer1969@gmail.com>
2020-03-13 08:32:15 -0700

    More batch.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,11 @@
+set
 set QT_BASE_X64="C:\Qt\5.14\msvc2017_64"
 set PYTHON3_BASE_X64="C:\Miniconda36-x64"
 dir
-msbuild nionui-tool-0.4.0\launcher\NionUILauncher.vcxproj
-REM ps: Get-ChildItem -Include *.pdb -Recurse | foreach { $_.Delete() }  # [win]
-REM ps: Get-ChildItem launcher\x64\Release\imageformats -Include *d.dll -Recurse | foreach { $_.Delete() }  # [win]
-%PYTHON3_BASE_X64%\python.exe setup.py bdist_wheel
-%PYTHON3_BASE_X64%\python.exe -m pip install --no-deps  --ignore-installed dist\\{{ fn }} -vv
+msbuild launcher\NionUILauncher.vcxproj
+dir dist\
+powershell "Get-ChildItem -Include *.pdb -Recurse | foreach { $_.Delete() }"
+powershell "Get-ChildItem launcher\x64\Release\imageformats -Include *d.dll -Recurse | foreach { $_.Delete() }"
+%PYTHON% setup.py bdist_wheel
+dir dist\
+%PYTHON% -m pip install --no-deps  --ignore-installed dist\nionswift_tool-%PKG_VERSION%-cp36.cp37.cp38-none-win_amd64.whl -vv
