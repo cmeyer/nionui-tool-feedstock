472e048ae443c72cea42ffeb7fbbc2a3eb32f26a
Chris Meyer <cmeyer1969@gmail.com>
2020-09-25 09:20:47 -0700

    Use CMake to build conda version.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,37 @@
-%PYTHON% -m pip install --no-deps --ignore-installed https://github.com/nion-software/nionui-tool/releases/download/%PKG_VERSION%/nionui_tool-%PKG_VERSION%-cp36.cp37.cp38-none-win_amd64.whl
-if errorlevel 1 exit 1
+REM dir /S
+
+REM conda info
+REM conda list
+REM conda env list
+
+REM "%PYTHON%" --version
+
+REM copy /Y %RECIPE_DIR%\CMakeLists.txt launcher\CMakeLists.txt
+
+mkdir launcher\x64
+if errorlevel 1 exit /b 1
+
+pushd launcher\x64
+
+REM default python is from the activate conda environment which is not the build environment; specify python to cmake
+cmake -B. -S.. -DCMAKE_BUILD_TYPE:STRING=Release -DPython3_EXECUTABLE="%PYTHON%" -DQt5_DIR=C:\Qt\5.15\msvc2019_64\lib\cmake\Qt5
+if errorlevel 1 exit /b 1
+
+dir /S
+
+REM cmake --build . --config Release
+cmake --build . --config Release
+if errorlevel 1 exit /b 1
+
+dir /S
+
+rmdir /S /Q Release
+move build\Release Release
+if errorlevel 1 exit /b 1
+
+popd
+
+REM dir /S
+
+"%PYTHON%" -m pip install --no-deps --ignore-installed .
+if errorlevel 1 exit /b 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,48 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# install using pip from the whl files on PyPI
-
-if [ `uname` == Darwin ]; then
-    WHL_FILE=https://github.com/nion-software/nionui-tool/releases/download/${PKG_VERSION}/nionui_tool-${PKG_VERSION}-cp36.cp37.cp38-abi3-macosx_10_11_intel.whl
+if [[ "$target_platform" == linux* ]]; then
+  # set
+  # pwd
+  # ls -R .
+  # cp ${RECIPE_DIR}/CMakeLists.txt launcher/CMakeLists.txt
+  pushd launcher
+  # conda info
+  # conda list
+  # conda env list
+  # "${PYTHON}" --version
+  # default python is from the activate conda environment which is not the build environment; specify python to cmake
+  # sudo apt-get -y install libxfixes3
+  # sudo apt-get install qtbase5-dev
+  # sudo apt-get install libqt5svg5-dev
+  cmake CMakeLists.txt -DPython3_EXECUTABLE="$PYTHON" -DUSE_PYTHON_QT=OFF
+  cmake --build . --config Release
+  # ls -R .
+  # readelf -d build/NionUILauncher
+  # readelf -dr build/NionUILauncher | grep QAbstractItem
+  mkdir -p linux
+  mv build linux/x64
+  popd
+  "${PYTHON}" -m pip install --no-deps --ignore-installed .
 fi
 
-if [ `uname` == Linux ]; then
-    WHL_FILE=https://github.com/nion-software/nionui-tool/releases/download/${PKG_VERSION}/nionui_tool-${PKG_VERSION}-cp36.cp37.cp38-abi3-manylinux1_x86_64.whl
+if [[ "$target_platform" == "osx-64" ]]; then
+  # set
+  # pwd
+  # ls -R .
+  # cp ${RECIPE_DIR}/CMakeLists.txt launcher/CMakeLists.txt
+  pushd launcher
+  # conda info
+  # conda list
+  # conda env list
+  # "${PYTHON}" --version
+  # default python is from the activate conda environment which is not the build environment; specify python to cmake
+  brew install qt5
+  cmake CMakeLists.txt -DPython3_EXECUTABLE="$PYTHON" -DQt5_DIR=/usr/local/opt/qt/lib/cmake/Qt5
+  cmake --build . --config Release
+  # ls -R .
+  mkdir -p build/Release
+  mv build/*.app build/Release
+  popd
+  # ls -R .
+  "${PYTHON}" -m pip install --no-deps --ignore-installed .
 fi
-
-pip install --no-deps --ignore-installed $WHL_FILE
